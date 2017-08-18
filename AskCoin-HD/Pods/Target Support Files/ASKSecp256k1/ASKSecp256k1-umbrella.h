18061a0d39f2b4d2ddc0aea1ce2bb510620a45bd
HongYang Qiu <qiuhongyang@askcoin.org>
2017-08-18 15:25:00 +0800

    extended pub key ok

---
+++ AskCoin-HD/Pods/Target Support Files/ASKSecp256k1/ASKSecp256k1-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CKScep256k1.h"
+
+FOUNDATION_EXPORT double ASKSecp256k1VersionNumber;
+FOUNDATION_EXPORT const unsigned char ASKSecp256k1VersionString[];
+
