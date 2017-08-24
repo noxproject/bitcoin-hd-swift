d055299e259a0854b448cd18e5d9f7f7de6eb624
HongYang Qiu <qiuhongyang@askcoin.org>
2017-08-24 11:38:28 +0800

    base sign and verify

--- AskCoin-HD/AskCoin-HD/AskCoin-HD/ASKKey.swift
+++ AskCoin-HD/AskCoin-HD/AskCoin-HD/ASKKey.swift
@@ -9,7 +9,14 @@
 import UIKit
 import ASKSecp256k1
 
-// TODO: - Verify and signature
+enum ASKKeyVerifyResult: Int, Error {
+	case signatureInvalid = -4
+	case publicKeyInvalid = -3
+	case invalid = 0
+	case valid = 1
+}
+
+// TODO: - BTC sign and verify
 
 class ASKKey: NSObject {
 	static func generatePublicKey(with privateKey: Data, compression: Bool = true) -> Data {
@@ -19,4 +26,29 @@ class ASKKey: NSObject {
 		}
 		return data
 	}
+	
+	static func sign(data: Data, privateKey: Data) -> Data {
+		var data = Data()
+		if let t = CKSecp256k1.compactSign(data, withPrivateKey: privateKey) {
+			data.append(t)
+		}
+		return data
+	}
+	
+	static func verify(sigData: Data, srcData: Data, publicKey: Data) throws -> Bool {
+		let verifyResult = CKSecp256k1.verifySignedData(sigData, withMessageData: srcData, usePublickKey: publicKey)
+		
+		if let r = ASKKeyVerifyResult(rawValue: verifyResult) {
+			if r == .valid {
+				return true
+			}
+			else
+			{
+				throw r
+			}
+		}
+		
+		return false
+	}
+	
 }
